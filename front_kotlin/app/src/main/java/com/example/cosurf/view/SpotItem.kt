232393964b22820af893a22dfd7a423866625e0c
clara <clara.jacot@gmail.com>
2024-04-30 14:56:17 +0200

    amélioration View spotitem

--- front_kotlin/app/src/main/java/com/example/cosurf/view/SpotItem.kt
+++ front_kotlin/app/src/main/java/com/example/cosurf/view/SpotItem.kt
@@ -1,7 +1,10 @@
 package com.example.cosurf.view
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,30 +24,51 @@ import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 @Composable
-fun SpotItem(fullName: String, address: String) {
-    Row(verticalAlignment = Alignment.CenterVertically,
+fun SpotItem(name: String, address: String, picture: String) {
+    Column(
+
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { }
+            .padding(top = 20.dp)
+            .clickable { },
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(modifier = Modifier
-            .size(50.dp)
+       Box(modifier = Modifier
+            .size(350.dp)
             .padding(8.dp)
-            .clip(RoundedCornerShape(50))
+           .shadow(6.dp)
+            .border(2.dp, Color.Black )
             .background(Color(Random.nextInt()))
-            ){
-            Text(text = fullName.first().uppercase(),
+        ) {
+            Text(text = picture,
                 style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier.align(Alignment.Center)
             )
 
+
             }
-        Text(
-            text = fullName,
-            style = MaterialTheme.typography.bodyLarge,
+        Text(modifier = Modifier.padding(top = 20.dp),
+            text = name,
+            style = MaterialTheme.typography.titleLarge,
             fontWeight = FontWeight.Bold)
         Text(
             text = address,
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(modifier = Modifier.padding(top = 20.dp),
+            text = "Description :",
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(modifier = Modifier.padding(top = 20.dp),
+            text = "Nombre de requins :",
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(modifier = Modifier.padding(top = 20.dp),
+            text = "Température de l'eau :",
             style = MaterialTheme.typography.bodyMedium,
             fontWeight = FontWeight.SemiBold
         )
@@ -54,6 +79,7 @@ fun SpotItem(fullName: String, address: String) {
 @Composable
 fun SpotItemPreview() {
     SpotItem(
-        fullName = "Reef Break ",
-        address = "Pipeline, Oahu, Hawaii")
+        name = "Reef Break ",
+        address = "Pipeline, Oahu, Hawaii",
+        picture = "urlImage")
 }

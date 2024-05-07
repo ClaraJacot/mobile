2676c7e1630a47b1df74ed14645e64b3f53ba051
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-05-07 16:16:07 +0200

    refactoring

---
+++ front_kotlin/app/src/main/java/com/example/cosurf/MyMainScreen.kt
@@ -0,0 +1,35 @@
+package com.example.cosurf
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.cosurf.model.data.Welcome
+import com.example.cosurf.ui.theme.CosurfTheme
+import com.example.cosurf.view.AllSurfSpots
+import com.example.cosurf.view.SpotItem
+
+@Composable
+fun MyMainScreen(navController: NavHostController, surfSpotState: Welcome?) {
+    NavHost(navController = navController, startDestination = "allSpots") {
+        composable("allSpots") {
+            CosurfTheme {
+                surfSpotState?.let { surfSpot ->
+                    AllSurfSpots(navController = navController, welcome = surfSpot)
+                }
+            }
+        }
+        composable("spotItem/{spotId}") { backStackEntry ->
+            val spotId = backStackEntry.arguments?.getString("spotId")
+            val surfSpot = surfSpotState?.records?.firstOrNull { it.id.toString() == spotId }
+            surfSpot?.let { spot ->
+                CosurfTheme {
+                    SpotItem(
+                        surfSpot = spot,
+                        onBackClicked = { navController.popBackStack() }
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ front_kotlin/app/src/main/java/com/example/cosurf/SurfSpotViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.cosurf
+
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.cosurf.Api.ApiService
+import com.example.cosurf.Api.Constants
+import com.example.cosurf.model.data.Welcome
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
+
+class SurfSpotViewModel : ViewModel() {
+    private val _surfSpotState = mutableStateOf<Welcome?>(null)
+    val surfSpotState: State<Welcome?> = _surfSpotState
+
+    init {
+        fetchSurfSpots()
+    }
+
+    private fun fetchSurfSpots() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val apiService = retrofit.create(ApiService::class.java)
+
+        val result = apiService.fetchSpots(token = "Bearer " + Constants.API_KEY)
+        Log.d("lol", "$result")
+        result.enqueue(object : Callback<Welcome> {
+            override fun onResponse(call: Call<Welcome>, response: Response<Welcome>) {
+                if (response.isSuccessful) {
+                    _surfSpotState.value = response.body()
+                }
+            }
+
+            override fun onFailure(call: Call<Welcome>, t: Throwable) {
+                Log.d("erreur", "erroooooooor")
+
+            }
+        })
+    }
+}
+

588307163abf8cb6c5cfe793c37293e88b75fb44
clara <clara.jacot@gmail.com>
2024-05-28 14:27:30 +0200

    correction sécurité

--- front_kotlin/app/src/main/java/com/example/cosurf/Api/Constants.kt
+++ front_kotlin/app/src/main/java/com/example/cosurf/Api/Constants.kt
@@ -2,6 +2,4 @@ package com.example.cosurf.Api
 
 object Constants {
     const val BASE_URL = "http://10.0.2.2:8080/spots/"
-    const val SPOTS_URL = "Surf%20Destinations?maxRecords=3&view=By%20Surf%20Break"
-    const val API_KEY = "patPIPEm7EdAdNSeP.25be34760af7640047ea69660279d9ee93765bd4956fadff0e880cf422321d1c"
 }
